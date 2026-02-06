9512830b2cf2f3ff9e003b1061c7a1c5700e8c3c
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2026-02-07 05:36:03 +0630

    Add setup instructions for the application

---
+++ app.py
@@ -0,0 +1,9 @@
+# ၁။ Repo ကို Clone လုပ်ပါ
+!git clone https://github.com/YOUR_USERNAME/wan2.2-kaggle-t4.git
+%cd wan2.2-kaggle-t4
+
+# ၂။ လိုအပ်တဲ့ library တွေသွင်းပါ
+!pip install -r requirements.txt
+
+# ၃။ App ကို run ပါ
+!python app1.py
